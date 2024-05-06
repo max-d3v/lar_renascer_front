f82e240995d933029bf853063493d0d4ce635e2b
max <maxbuzzarello@gmail.com>
2024-05-05 21:13:12 -0300

    lógica básica de login lado usuario

--- lar-renascer/src/pages/login.tsx
+++ lar-renascer/src/pages/login.tsx
@@ -2,13 +2,19 @@ import { Input } from "../components/input"
 import { Button } from "../components/button"
 import { useForm } from "react-hook-form"
 import { useState } from "react";
-import { ajax } from "../ajax/ajax.ts";
+import { ajax } from "../ajax/ajax";
+import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from 'react-hot-toast' 
+
 export function Login() {
     const { register, handleSubmit } = useForm();
     const [errors, setErrors] = useState<any>({})
-
+    const navigate = useNavigate();
 
     const onSubmit = async (data: any) => {
+        toast.dismiss();
+        toast.loading("Autenticando");
+
         const {usuario, senha} = data;
         if (!usuario || usuario == "") {
             errors.usuario = "Usuário é obrigatório"
@@ -18,7 +24,21 @@ export function Login() {
         }
 
         const response = await ajax("/auth/login", "post", data); 
-        
+        if (!response) {
+            toast.dismiss();
+            toast.error("Erro inesperado!");
+        }
+        if (response.status == "error") {
+            toast.dismiss();
+            toast.error(response.message);
+        }
+        if (response.status == "success") {
+            toast.dismiss();
+            navigate("/");
+        }
+
+        toast.dismiss();
+        toast.error("Erro inesperado!");
 
 
 
@@ -32,7 +52,7 @@ export function Login() {
                     <img src="../public/images/renascer_logo.png" alt="" />
                 </div>
                 <form action="" onSubmit={handleSubmit(onSubmit)}>
-                <div className="gap-8 mt-12  flex flex-col w-full">
+                <div className="gap-4 mt-12  flex flex-col w-full">
                     <Input register={register} name="usuario" placeholder="Usuário" title="Usuário" />
                     <Input register={register} name="senha" placeholder="Senha" title="Senha" />
                 </div>
@@ -41,6 +61,7 @@ export function Login() {
                 </div>
                 </form>
             </div>
+            <Toaster></Toaster>
         </div>
     )
 }
