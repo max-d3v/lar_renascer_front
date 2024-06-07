f24924f7801eed44f93dec647093ffcef5ae0e35
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-06-07 19:37:20 -0300

    Benfreitores register, both juridico and fisico

--- lar-renascer/src/pages/benfeitoresRegistro.tsx
+++ lar-renascer/src/pages/benfeitoresRegistro.tsx
@@ -2,12 +2,15 @@ import { useParams } from "react-router-dom"
 import { Input } from "../components/input";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Button } from "../components/button";
+import { ajax, apiResponse } from "../ajax/ajax.mts";
+import toast, {Toaster} from 'react-hot-toast';
 export function BenfeitoresRegister() {
     const [errors, setErrors] = useState<any>({});
     const { tipo } = useParams();
     const { register, handleSubmit } = useForm(); 
-
+    const navigate = useNavigate();
     if (!tipo || (tipo !== "fisica" && tipo !== "juridica")) {
         return (
             <div className="customPink pageHeight w-screen flex justify-center items-center font-semibold text-3xl">
@@ -18,10 +21,44 @@ export function BenfeitoresRegister() {
 
 
     const onSubmit = async (data: any) => {
-        console.log(data);
+        toast.dismiss();
+        toast.loading("Cadastrando");
+        if (tipo !== "fisica" && tipo !== "juridica") {
+            toast.dismiss();
+            return toast.error("Tipo de benfeitor inválido!");
+        }
+        const response:apiResponse = await ajax(`/benfeitores/register${tipo}`, "post", data);
+        toast.dismiss();
+        if (response.status == "error") {
+            if (Array.isArray(response.message)) {
+                const newErrors = { ...errors }; // Crie uma cópia do estado errors
+                response.message.forEach((error: any) => {
+                    newErrors[error.path] = error.msg;
+                });
+                setErrors(newErrors); // Defina o estado com o novo objeto
+                console.log(newErrors);
+                return;
+            }
+            if (typeof response.message == "string") {
+                return toast.error(response.message);
+            }
+            return toast.error("Erro inesperado!");
+            }
+        if (response.status == "success") {
+            toast.success("Cadastro Realizado!");
+            setTimeout(() => {
+                return navigate(-1);    
+            }, 3000);
+            return;
+        }
+
+        toast.error("Erro inesperado!");
+        return;
     }
 
     return (
+        <>
+        <Toaster/>
         <div className="customPink pageHeight w-screen flex justify-center items-center font-semibold text-3xl">
             <form className="flex flex-col w-4/6 h-4/6" onSubmit={handleSubmit(onSubmit)} >
                 <div className="self-start flex flex-col gap-12 " >
@@ -43,13 +80,16 @@ export function BenfeitoresRegister() {
                     }
                     
                 </div>
-                <div className="mt-12 w-1/6 " >
-                    <Button name="cadastrar" title="Cadastrar" type="submit" /> 
-                </div>
-                <div>
-                    <Button></Button>
+                <div className="mt-12 w-full flex items-center justify-between  " >
+                    <div className="w-2/6" >
+                        <Button name="cadastrar" title="Cadastrar" type="submit" /> 
+                    </div>
+                    <div className="w-2/6" >
+                        <Button customCss="bg-red-500" name="cancelar" title="Cancelar" onClick={ () => navigate(-1) }  />
+                    </div>
                 </div>
             </form>
         </div>
+        </>
     )
 }
